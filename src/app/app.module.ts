0ef526f910be1a78bebd5e7b2319fdcb5eec3153
Mehrez Hrizi <mehrez.hrizi.official@gmail.com>
2022-04-19 19:54:35 +0100

    add support for OAuth 2 and OpenID Connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { LayoutModule, LogoModule } from '@backbase/ui-ang';
+
+import { OAuthModule, AuthConfig, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
+
+import { authConfig } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +19,26 @@ import { LayoutModule, LogoModule } from '@backbase/ui-ang';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    LogoModule
+    LogoModule,
+    OAuthModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthConfig,
+      useValue: authConfig
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [ OAuthService ],
+      useFactory: (oAuthService: OAuthService) => () =>
+        oAuthService.loadDiscoveryDocumentAndTryLogin().then(() =>
+          oAuthService.setupAutomaticSilentRefresh()
+        )
+    },
+    { provide: OAuthStorage, useFactory: () => localStorage },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { AuthConfig } from "angular-oauth2-oidc";
+
 export const environment = {
   production: false
 };
@@ -14,3 +16,26 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+export const authConfig: AuthConfig = {
+  // Url of the Identity Provider
+  issuer: 'https://identity.prd.sdbx.live.backbaseservices.com/auth/realms/retail',
+  // URL of the SPA to redirect the user to after login
+  redirectUri: document.location.origin + '/',
+  // The SPA's id. The SPA is registered with this id at the auth-server
+  clientId: 'bb-web-client',
+  // Just needed if your auth server demands a secret. In general, this
+  // is a sign that the auth server is not configured with SPAs in mind
+  // and it might not enforce further best practices vital for security
+  // such applications. (IE: does not support PKCE)
+  // dummyClientSecret: 'secret',
+  responseType: 'code',
+  // set the scope for the permissions the client should request
+  // The first four are defined by OIDC.
+  // Important: Request offline_access to get a refresh token
+  scope: 'openid',
+  requireHttps: false,
+  showDebugInformation: true,
+  logoutUrl: document.location.origin + '/login',
+};
